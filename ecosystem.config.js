74f23abf0482bf2ba695b8c12f20500accd87859
Nawoda Hansani <nawodahansani13@gmail.com>
2026-03-31 09:15:35 +0530

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [
       {
+          name: "cloud-sql-auth-proxy",
+          script: "./cloud-sql-proxy project-042ca024-2846-46b6-b36:asia-southeast1:mysql-vm project-042ca024-2846-46b6-b36:asia-southeast1:postgres-vm --private-ip",
+          log_file: "./logs/cloud-sql=proxy.log"
+      },
+      {
           name   : "student-service",
           script : "java -jar ./student-service/target/Student-Service-1.0.0.jar",
           log_file: "./logs/student-service.log",
